c1bad3280ac934f3a62f878e741cf663d4682531
Rajeshkannan Palvannan <rajeshkannan@heptagon.in>
2019-08-25 08:30:45 +0530

    test

--- server/models/bill.ts
+++ server/models/bill.ts
@@ -3,6 +3,7 @@ import * as mongoose from 'mongoose';
 const billSchema = new mongoose.Schema({
   amount: Number,
   works: [],
+  spares: [],
   customerName: String,
   vehicleNumber: String,
   phoneNumber: String
